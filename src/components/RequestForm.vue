1d389e5e158997a3500bb6d68a8d2346cb23aa46
Viktor Denikin <leshden@yandex.ru>
2022-06-03 14:12:33 +0300

    check fill regions and groups

--- src/components/RequestForm.vue
+++ src/components/RequestForm.vue
@@ -1,8 +1,12 @@
 <template>
   <form @submit.prevent='' class='request-form'>
-    <select class='regions-form'>
-      <option disabled value="">Выбрать регион...</option>
-      <option v-for="region in $store.state.regions" :value='region.name' :key='region.id'>{{region.name}}</option>
+    <select class='regions-form' @change='onSelectRegion'>
+      <option disabled value="" selected>Выбрать регион...</option>
+      <option v-for="region in $store.state.regions" :value='JSON.stringify({id: region.id,  name: region.name})' :key='region.id'>{{region.name}}</option>
+    </select>
+    <select class='groups-form' @change='onSelectGroup'>
+      <option disabled value="" selected>Выбрать группу...</option>
+      <option v-for="group in $store.state.groups.content" :value='JSON.stringify({id: group.id,  name: group.name})' :key='group.id'>{{group.name}}</option>
     </select>
     <input type='submit' value='Отправить заявку'>
   </form>
@@ -10,8 +14,33 @@
 
 <script lang='ts'>
 import {Vue} from 'vue-class-component';
+import {isEmptyObject} from '../utils/utils';
 
 export default class RequestForm extends Vue {
+  public selectedRegion = {}
+  public selectedGroup = {}
+
+  public onSelectGroup(e: any): void {
+    if(e.target.options.selectedIndex > -1) {
+      this.selectedGroup = JSON.parse(e.target.options[e.target.options.selectedIndex].value);
+      this.checkRequestType();
+    }
+  }
+
+  public onSelectRegion(e: any): void {
+    if(e.target.options.selectedIndex > -1) {
+      this.selectedRegion = JSON.parse(e.target.options[e.target.options.selectedIndex].value);
+      this.checkRequestType();
+    }
+  }
+
+  public checkRequestType(): void {
+    if (isEmptyObject(this.selectedRegion) || isEmptyObject(this.selectedGroup)) {
+      return;
+    }
+
+    console.log('Get request Types');
+  }
 }
 </script>
 
