6eaa460eaf161020fa7c5e5772f510cab5168e3d
Viktor Denikin <leshden@yandex.ru>
2022-06-04 17:08:58 +0300

    remove old project code

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,17 @@
   <div class='app'>
     <h1>{{title}}</h1>
     <RequestForm />
-    <TodoInput />
-    <TodoList />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import TodoInput from './components/TodoInput.vue';
-import TodoList from './components/TodoList.vue';
 import RequestForm from './components/RequestForm.vue';
 import {loadData, saveData} from './storage-data/storage-data';
 import {getDataFromServer} from './get-requests/get-requests';
 
 @Options({
   components: {
-    TodoInput,
-    TodoList,
     RequestForm
   },
 })
--- src/store/index.ts
+++ src/store/index.ts
@@ -7,18 +7,11 @@ interface Region {
 
 export default createStore({
   state: {
-    todos: ['String1', 'String2', 'String3'],
     regions: [],
     groups: {},
     types: {}
   },
   mutations: {
-    ADD_TODO (state, todo: string) {
-      state.todos.push(todo)
-    },
-    DELETE_TODO (state, todo: string) {
-      state.todos = state.todos.filter(t=> t!== todo)
-    },
     UPDATE_REGIONS (state, regions: any) {
       state.regions = regions;
     },
@@ -30,12 +23,6 @@ export default createStore({
     }
   },
   actions: {
-    addTodo({commit}, todo: string) {
-      commit('ADD_TODO', todo);
-    },
-    deleteTodo({commit}, todo: string) {
-      commit('DELETE_TODO', todo);
-    },
     updateRegions({commit}, regions: any) {
       commit('UPDATE_REGIONS', regions);
     },
